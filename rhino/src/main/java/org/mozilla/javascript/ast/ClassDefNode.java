522c10813266c2db6fc1f4f992c5ee743615e7c3
andrea.bergia <andrea.bergia@servicenow.com>
2025-02-04 17:44:53 +0100

    feat(class): parsing constructors
    Copy&pasted lots of code from object literals, so many other things
    should work "for free". Tests next! :-)

--- rhino/src/main/java/org/mozilla/javascript/ast/ClassDefNode.java
+++ rhino/src/main/java/org/mozilla/javascript/ast/ClassDefNode.java
@@ -3,21 +3,27 @@ package org.mozilla.javascript.ast;
 import org.mozilla.javascript.Token;
 
 public class ClassDefNode extends ScriptNode {
-	private final Name className;
+    private final Name className;
+    private FunctionNode constructor;
 
-	public ClassDefNode(
-			int pos,
-			Name name
-	) {
-		super(pos);
-		this.className = name;
-	}
+    public ClassDefNode(int pos, Name name) {
+        super(pos);
+        this.className = name;
+    }
 
-	{
-		type = Token.CLASS;
-	}
+    {
+        type = Token.CLASS;
+    }
 
-	public Name getClassName() {
-		return className;
-	}
+    public Name getClassName() {
+        return className;
+    }
+
+    public FunctionNode getConstructor() {
+        return constructor;
+    }
+
+    public void setConstructor(FunctionNode constructor) {
+        this.constructor = constructor;
+    }
 }
