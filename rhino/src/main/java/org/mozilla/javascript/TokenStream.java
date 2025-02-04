4fce5785808c806409a2c713395add6a41a9c7e2
andrea.bergia <andrea.bergia@servicenow.com>
2025-02-04 16:24:18 +0100

    feat(class): start parsing declaration

--- rhino/src/main/java/org/mozilla/javascript/TokenStream.java
+++ rhino/src/main/java/org/mozilla/javascript/TokenStream.java
@@ -360,7 +360,7 @@ class TokenStream implements Parser.CurrentPositionReporter {
                 Id_break = Token.BREAK,
                 Id_case = Token.CASE,
                 Id_catch = Token.CATCH,
-                Id_class = Token.RESERVED,
+                Id_class = Token.CLASS,
                 Id_const = Token.CONST,
                 Id_continue = Token.CONTINUE,
                 Id_debugger = Token.DEBUGGER,
@@ -411,7 +411,8 @@ class TokenStream implements Parser.CurrentPositionReporter {
                 // Non ReservedWord, but Non IdentifierName in strict mode code.
                 // 12.1.1 Static Semantics: Early Errors
                 Id_let = Token.LET, // TODO : Valid IdentifierName in non-strict mode.
-                Id_static = Token.RESERVED;
+                Id_static = Token.STATIC,
+                Id_constructor = Token.CONSTRUCTOR;
 
         int id = 0;
         switch (name) {
@@ -566,6 +567,9 @@ class TokenStream implements Parser.CurrentPositionReporter {
                 if (isStrict) {
                     id = Id_static;
                 }
+                break;
+            case "constructor":
+                id = Id_constructor;
                 break;
             default:
                 id = 0;
